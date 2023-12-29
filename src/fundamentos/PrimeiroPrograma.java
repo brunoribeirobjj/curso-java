9a8b73b42c1098cde3f717329793b11f35f25f78
Bruno Ribeiro <brunoribeirobjj@gmail.com>
2023-12-29 01:03:38 -0300

    Fundamentos Java concluído

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,31 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Primeiro Programa Parte #01!");
+		System.out.println("Primeiro Programa Parte #01!");
+		System.out.println("Primeiro Programa Parte #03!"); //exemplo de comentário
+		
+		//Uma sentença de código termina em ;
+		
+		/* Comentários de multiplas linhas
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3
+		 * */
+		
+		// Cotrol + / comenta linha ou selecionando multiplas linhas comenta simultaneamente
+		
+			
+		/**
+		 * JAVADOC
+		 */
+		
+	
+		
+		
+		
+	}
+}
+
